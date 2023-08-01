dcec5171d0a3246f3afedfb3fb5f8653b91a354b
Yurick <YJakse789@gmail.com>
2023-08-01 12:49:54 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(depth){
   // Write your code here!
+if (depth <= 400) {
+  return "This one is on me!"
+} else {
+  if (depth >= 2000) {
+    if (depth > 2500) {
+      return "No can do."
+    } else {
+      return "I will gladly take your thirty bucks."
+    }
+  }
+  else {
+    return "That will be twenty bucks."
+  }
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much."
+  }
+  if (tip === "not as generous") {
+    return "Thank you."
+  }
+  if (tip !== "generous" && tip !== "not as generous") {
+    return "Bye."
+  }
 }
